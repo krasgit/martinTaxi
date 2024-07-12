debee70caa7a8c043ffd689afcd5bd92a64131e3
sn1wfy <martinnikolov688@gmail.com>
2024-07-12 20:28:09 +0200

    Има redirect, преименувах index.html, и tracking страница, която НЕ е завършена

--- js.js
+++ js.js
@@ -1,7 +1,7 @@
 function initMap() {
     var options = {
         zoom: 8,
-        center: { lat: -34.397, lng: 150.644 }
+        center: { lat: 43.212030, lng:  27.900408 }
     }
     var map = new google.maps.Map(document.getElementById('map'), options);
 }
